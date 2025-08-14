34e196a93128e705447811195193772a27ba49fb
Esraa Mohsen <esraamohsan239@gmail.com>
2025-08-14 16:33:54 +0300

    Add Basic Files

---
+++ lib/Constants/constants.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color(0xff2B475E);
+const kSecondaryColor = Color.fromARGB(255, 18, 168, 56);
+const kLogo = 'assets/images/scholar.png';
+const kMessagesCollections = 'messages';
+const kMessage = 'message';
+const kCreatedAt = 'createdAt';
+const kDefultPadding = 10.0;
+
+TextStyle textStyle = const TextStyle(
+  fontSize: 30,
+  fontWeight: FontWeight.bold,
+  color: Colors.white,
+);
+
+//flutter run -d all
